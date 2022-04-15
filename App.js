efafe3215b23c3015f2ab1612ad18dc9acb40515
Anh Cao <anhcao563@gmail.com>
2022-04-15 18:47:50 +0300

    Add Doctors data in json format

--- App.js
+++ App.js
@@ -4,11 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-import AppointmentScreen from "./components/AppointmentScreen";
-import DietScreen from "./components/DietScreen";
-import HomeScreen from "./components/HomeScreen";
-import InfoScreen from "./components/InfoScreen";
-import ProfileScreen from "./components/ProfileScreen";
+import AppointmentScreen from "./components/screens/AppointmentScreen";
+import DietScreen from "./components/screens/DietScreen";
+import HomeScreen from "./components/screens/HomeScreen";
+import ProfileScreen from "./components/screens/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -24,8 +23,6 @@ export default function App() {
               iconName = "md-home-outline";
             } else if (route.name === "Appointment") {
               iconName = "md-calendar-outline";
-            } else if (route.name === "Info") {
-              iconName = "md-information-circle-outline";
             } else if (route.name === "Diet") {
               iconName = "md-restaurant-outline";
             } else if (route.name === "Profile") {
@@ -42,7 +39,6 @@ export default function App() {
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Appointment" component={AppointmentScreen} />
-        <Tab.Screen name="Info" component={InfoScreen} />
         <Tab.Screen name="Diet" component={DietScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
---
+++ components/consts/Doctor.js
@@ -0,0 +1,147 @@
+const doctors = [
+  {
+    id: "1",
+    firstname: "Claire",
+    lastname: "Baker",
+    professional: "General Practioner",
+    image: require("../assets/General_Practioner_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "31 years",
+    details:
+      "Hi, my name is Claire Backer and my professional is General Practioner. I have 31 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "2",
+    firstname: "John",
+    lastname: "Johnson",
+    professional: "General Practioner",
+    image: require("../assets/General_Practioner_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "25 years",
+    details:
+      "Hi, my name is John Johnson and my professional is General Practioner. I have 25 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "3",
+    firstname: "Mary",
+    lastname: "Philips",
+    professional: "Dermatologist",
+    image: require("../assets/Dermatologist_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "30 years",
+    details:
+      "Hi, my name is Mary Philips and my professional is Dermatologist. I have 30 years experience in this area that I promise I can help you to improve your current skin",
+  },
+  {
+    id: "4",
+    firstname: "Dan",
+    lastname: "Davidson",
+    professional: "Internale Medicine",
+    image: require("../assets/Internale_Medicine_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "20 years",
+    details:
+      "Hi, my name is Dan Davidson and my professional is Internale Medicine. I have 20 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "5",
+    firstname: "Emma",
+    lastname: "Baker",
+    professional: "Internale Medicine",
+    image: require("../assets/Internale_Medicine_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "28 years",
+    details:
+      "Hi, my name is Emma Backer and my professional is Internale Medicine. I have 28 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "6",
+    firstname: "Shi",
+    lastname: "Zhuang",
+    professional: "Herbal Medicine",
+    image: require("../assets/Herbal_Medicine_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "11 years",
+    details:
+      "Hi, my name is Shi Zhuang and my professional is Herbal Medicine. I have 11 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "7",
+    firstname: "Long",
+    lastname: "Lee",
+    professional: "Herbal Medicine",
+    image: require("../assets/Herbal_Medicine_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "16 years",
+    details:
+      "Hi, my name is Long Lee and my professional is Herbal Medicine. I have 16 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "8",
+    firstname: "Erick",
+    lastname: "Wilfrid",
+    professional: "Infectious Disease",
+    image: require("../assets/Infectious_Disease_Male_2.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "22 years",
+    details:
+      "Hi, my name is Erick Wilfrid and my professional is Infectious Disease. I have 22 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "9",
+    firstname: "Bale",
+    lastname: "Orrell",
+    professional: "Infectious Disease",
+    image: require("../assets/Infectious_Disease_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "31 years",
+    details:
+      "Hi, my name is Bale Orrell and my professional is Infectious Disease. I have 31 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "10",
+    firstname: "Tylar",
+    lastname: "Gareth",
+    professional: "Surgeon",
+    image: require("../assets/Surgeon_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "35 years",
+    details:
+      "Hi, my name is Tylar Gareth and my professional is Surgeon. I have 35 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "11",
+    firstname: "Taylor",
+    lastname: "Baker",
+    professional: "Surgeon",
+    image: require("../assets/Surgeon_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "29 years",
+    details:
+      "Hi, my name is Taylor Backer and my professional is Surgeon. I have 29 years experience in this area that I promise I can help you with your health problems",
+  },
+  {
+    id: "12",
+    firstname: "Leyla",
+    lastname: "Elsi",
+    professional: "Veterinarian",
+    image: require("../assets/Veterinarian_Female.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "5 years",
+    details:
+      "Hi, my name is Leyla Elsi and my professional is Veterinarian. I have 5 years experience in this area that I promise I can help you with your sweties",
+  },
+  {
+    id: "13",
+    firstname: "Kelvin",
+    lastname: "Dublin",
+    professional: "Veterinarian",
+    image: require("../assets/Veterinarian_Male.png"),
+    description: "Doctor of the highest qualification category",
+    experiences: "11 years",
+    details:
+      "Hi, my name is Kelvin Dublin and my professional is Veterinarian. I have 11 years experience in this area that I promise I can help you with your pets",
+  },
+];
+
+export default doctors;
